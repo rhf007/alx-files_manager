c7912e7f8018e12194795da0a568bc197758295f
Rahaf Hazem <110139745+rhf007@users.noreply.github.com>
2024-12-09 18:39:08 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
@@ -12,6 +13,7 @@ router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', AuthController.getMe);
+router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);  
 
 module.exports = router;
