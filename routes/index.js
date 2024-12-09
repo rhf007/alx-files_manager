6c47fdbefeb80ab03721b740241de95f98dea10e
Rahaf Hazem <110139745+rhf007@users.noreply.github.com>
2024-12-09 15:21:37 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,18 @@ function controllerRouting(app) {
   const router = express.Router();
   app.use('/', router);
 
+  // App Controller
+
+  // should return if Redis is alive and if the DB is alive
   router.get('/status', (req, res) => {
     AppController.getStatus(req, res);
   });
 
+  // should return the number of users and files in DB
   router.get('/stats', (req, res) => {
     AppController.getStats(req, res);
   });
 
 }
+
 export default controllerRouting;
